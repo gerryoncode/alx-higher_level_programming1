ba86a0f044aa89354d1ce31858d35440400befbd
Gerald <gerryoncode@gmail.com>
2023-05-10 11:58:35 -0700

    Made changes to pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
-    else:
-        print('{:c}'.format(90 - i), end='')
+def pow(a, b):
+    return a ** b
