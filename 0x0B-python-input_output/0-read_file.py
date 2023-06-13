1ed3a3dae9d95e549cbf62cb27dbac38b7ad3605
Gerald <gerryoncode@gmail.com>
2023-06-13 10:45:51 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that reads a test file in utf-8 format and prints it to stdout
+"""
+def read_file(filename=""):
+    """ Function takes a filename and reads the data """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+    f.close()
+    print(read_data)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+this is the 0x0b python input and output tasks
