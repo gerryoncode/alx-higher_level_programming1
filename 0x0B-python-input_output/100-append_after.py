c7c8cdb8c455841bef087a8bd7cea4a8ea0761a6
Gerald <gerryoncode@gmail.com>
2023-06-14 07:00:49 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Function that inserts a line of test to a file after each line containing a specific string"""
+
+
 def append_after(filename="", search_string="", new_string=""):
     """Adds a new line after each line containing aspecific string"""
    text = ""
-   with open(filename, 'r', encoding="utf-8") as f:
-        for line in r:
+   with open(filename, 'r') as f:
+        for line in f:
             text += line
             if search_string in line:
                 text += string
