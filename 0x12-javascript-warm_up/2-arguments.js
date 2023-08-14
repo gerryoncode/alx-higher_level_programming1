d310f0c6686a7515477eb94217119319199d5de9
Gerald <gerryoncode@gmail.com>
2023-08-14 07:32:18 -0700

    Minor changes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let args = process.argv.slice(2);
-if (args.length < 1) {
-	console.log("No argument");
-} else if (args.length < 2) {
-	console.log("Argument found");
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
